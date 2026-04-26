78e6d8df5833c0a07856a210ec4d88f586818c70
arturaPortativais <arturs.berovskis@gmail.com>
2026-04-26 21:11:08 +0300

    Izveidoti visi nepieciešamie modeļi. Modeļi nav vēl aizpildīti, bet migrācijas ir.

---
+++ database/migrations/2026_04_26_174343_create_banks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('banks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50)->unique();
+            $table->text('description');
+            $table->boolean('public');
+            $table->boolean('collaborative');
+            $table->boolean('hidden');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('banks');
+    }
+};
---
+++ database/migrations/2026_04_26_174539_create_results_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            $table->float('score');
+            $table->json('results');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('test_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('results');
+    }
+};
