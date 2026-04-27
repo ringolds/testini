3b466cbd2855bb9a6277e79e055b07f7d4fcde3b
arturaPortativais <arturs.berovskis@gmail.com>
2026-04-27 10:39:27 +0300

    Izlabota migrāciju secība, lai test būtu pirms atsaucēm uz to, izveidota jautājumu migrācija, kas tika nejauši izdzēsta pirms tam.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,10 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('google_id')->nullable()->unique();
+            $table->boolean('is_admin')->default(false);
+            $table->boolean('is_banned')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
