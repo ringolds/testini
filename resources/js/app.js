f2cfba85de6337656eacff54aeac9866dbdad8c5
arturaPortativais <arturs.berovskis@gmail.com>
2026-06-08 22:51:09 +0300

    js faili ielikti vite.config, iznemti no app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,1 @@
 //
-import './collection_manager.js';
-import './question_manager.js';
-import './map_manager.js';
-import './game_manager.js';
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 
+                'resources/js/app.js',
+                './collection_manager.js',
+                './question_manager.js',
+                './map_manager.js',
+                './game_manager.js'],
             refresh: true,
         }),
         tailwindcss(),
